624243e842518852357df04b5c2bbfb34180b6b9
Gabriel <gabriel.sadaka@outlook.com>
2014-11-25 22:11:14 +1100

    wrapped JSON ping in try/catch

--- XBMCRemoteRT/XBMCRemoteRT.Shared/RPCWrappers/JSONRPC.cs
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/RPCWrappers/JSONRPC.cs
@@ -13,33 +13,40 @@ namespace XBMCRemoteRT.RPCWrappers
     {
         public async static Task<bool> Ping(ConnectionItem connectionItem)
         {
-            JObject requestObject = new JObject(
-                new JProperty("jsonrpc", "2.0"),
-                new JProperty("id", 234),
-                new JProperty("method", "JSONRPC.ping"));
-                
-            string requestData = requestObject.ToString();
+            try
+            {
+                JObject requestObject = new JObject(
+                    new JProperty("jsonrpc", "2.0"),
+                    new JProperty("id", 234),
+                    new JProperty("method", "JSONRPC.ping"));
 
-            HttpClientHandler handler = new HttpClientHandler();
-            HttpClient httpClient = new HttpClient(handler);
-            
-            string uriString = "http://" + connectionItem.IpAddress + ":" + connectionItem.Port.ToString() + "/jsonrpc?request=";
-            httpClient.BaseAddress = new Uri(uriString);
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
+                string requestData = requestObject.ToString();
 
-            request.Content = new StringContent(requestData);
-            request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"); //Required to be recognized as valid JSON request.
+                HttpClientHandler handler = new HttpClientHandler();
+                HttpClient httpClient = new HttpClient(handler);
 
-            if (connectionItem.Password != String.Empty)
-                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes(String.Format("{0}:{1}", connectionItem.Username, connectionItem.Password))));
+                string uriString = "http://" + connectionItem.IpAddress + ":" + connectionItem.Port.ToString() + "/jsonrpc?request=";
+                httpClient.BaseAddress = new Uri(uriString);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "");
 
-            HttpResponseMessage response = await httpClient.SendAsync(request);
-            string responseString = await response.Content.ReadAsStringAsync();
-            if (responseString.Length == 0)
+                request.Content = new StringContent(requestData);
+                request.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json"); //Required to be recognized as valid JSON request.
+
+                if (connectionItem.Password != String.Empty)
+                    request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes(String.Format("{0}:{1}", connectionItem.Username, connectionItem.Password))));
+
+                HttpResponseMessage response = await httpClient.SendAsync(request);
+                string responseString = await response.Content.ReadAsStringAsync();
+                if (responseString.Length == 0)
+                    return false;
+                dynamic responseObject = JObject.Parse(responseString);
+                bool isSuccessful = responseObject.result == "pong";
+                return isSuccessful;
+            }
+            catch
+            {
                 return false;
-            dynamic responseObject = JObject.Parse(responseString);
-            bool isSuccessful = responseObject.result == "pong";
-            return isSuccessful;
+            }
         }
     }
 }
