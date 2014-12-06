cb09b46e8d94953983536581d8527a85a993d5dd
Akshay Zade <akshay2000@hotmail.com>
2014-12-06 17:45:13 +0530

    Added Constants file to git.

---
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Helpers/Constants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XBMCRemoteRT.Helpers
+{
+    public static class EventCategories
+    {
+        public const string UIInteraction = "UIInteraction";
+        public const string Programmatic = "Programmatic";
+    }
+
+    public static class EventActions
+    {
+        public const string Click = "Click";
+        public const string Play = "Play";
+    }
+
+    public static class EventNames
+    {
+        public const string PlayMovie = "PlayMovie";
+        public const string PlayEpisode = "PlayEpisode";
+        public const string PlayArtist = "PlayArtist";
+        public const string PlayAlbum = "PlayAlbum";
+        public const string PlaySong = "PlaySong";
+    }
+
+    public static class TimingCategories
+    {
+        public const string LoadTime = "LoadTime";
+    }
+}
