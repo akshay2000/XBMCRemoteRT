f8680e3e5264ab17b9e8eeff0e3f439973e3c10b
Akshay Zade <akshay2000@hotmail.com>
2014-12-08 23:12:49 +0530

    Fixed font rendering on about page.

--- XBMCRemoteRT/XBMCRemoteRT.Shared/Helpers/ConnectionManager.cs
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Helpers/ConnectionManager.cs
@@ -87,7 +87,6 @@ namespace XBMCRemoteRT.Helpers
             }
             else
             {
-                GlobalVariables.CurrentTracker.SendException("HttpRequest Failed", true);
                 MessageDialog msg = new MessageDialog("Make sure Kodi is running and you're connected.", "Server not found");
                 await msg.ShowAsync();
                 return new JObject();
