cf9347dc60a29ae2853be27cba901a974253d211
Akshay Zade <akshay2000@hotmail.com>
2014-12-15 18:02:08 +0530

    Fixed #7

--- XBMCRemoteRT/XBMCRemoteRT.Shared/Helpers/ConnectionManager.cs
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Helpers/ConnectionManager.cs
@@ -18,7 +18,6 @@ namespace XBMCRemoteRT.Helpers
     {
         /// <summary>
         /// Class for providing methods and members related to making and maintaining connections to the server.
-        /// An object is created from this class at the app initiation time (see App.xaml.cs) and used universally across the app.
         /// </summary>
         /// 
 
