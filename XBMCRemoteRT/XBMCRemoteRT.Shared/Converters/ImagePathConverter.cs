22c57b35d352e77e818abdc0f317de767d27b600
Christian Prescott <me@christianprescott.com>
2015-01-08 19:17:09 -0500

    Hash image URL into storage path
    Full image://... URI is hashed into the filename for the cached image.
    Images are stored in a flat structure in the app temp folder.
    Directories no longer traversed to match source.
    File is not created until HTTP request completes successfully.

--- XBMCRemoteRT/XBMCRemoteRT.Shared/Converters/ImagePathConverter.cs
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Converters/ImagePathConverter.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Data;
@@ -18,29 +19,19 @@ namespace XBMCRemoteRT.Converters
             string imagePath = (value == null) ? string.Empty : (string)value;
             string imageURL = string.Empty;
             string proxyScheme = "image://";
-            string httpScheme = "http://";
             // TODO: are there other possible schemes for imagePath?
+            // TODO: Only apply cache logic if user is using authentication
             if (imagePath.StartsWith(proxyScheme))
             {
-                string uri = imagePath.Substring(proxyScheme.Length);
-
-                // Transform URI to local storage path
-                string decodedUri = WebUtility.UrlDecode(uri).Trim('/');
-                if (decodedUri.StartsWith(httpScheme))
-                {
-                    // TODO: Proxy http sources through Kodi? Good for consistency I guess: decodedUri = decodedUri.Substring(httpScheme.Length);
-                    return decodedUri;
-                }
-                // TODO: I am running Kodi on Ubuntu. What needs to change for Windows paths?
-                string storagePath = decodedUri.Replace('/', '\\');
-                // TODO: What happens if the URI passed to XAML has invalid characters?
-                imageURL = ApplicationData.Current.TemporaryFolder.Path + '\\' + storagePath;
+                // Hash the image proxy path into local storage file name
+                string storageFileName = MD5Core.GetHashString(imagePath) + ".tmp";
+                imageURL = ApplicationData.Current.TemporaryFolder.Path + '\\' + storageFileName;
 
                 // Build Kodi proxy image address
-                var encodedUri = WebUtility.UrlEncode(uri);
-                string baseUrlString = "http://" + ConnectionManager.CurrentConnection.IpAddress + ":" + ConnectionManager.CurrentConnection.Port.ToString() + "/image/image://";
+                var encodedUri = WebUtility.UrlEncode(imagePath);
+                string baseUrlString = "http://" + ConnectionManager.CurrentConnection.IpAddress + ":" + ConnectionManager.CurrentConnection.Port.ToString() + "/image/";
                 Uri imageUri = new Uri(baseUrlString + encodedUri);
-                CheckImageStorage(storagePath, imageUri);
+                CacheImage(imageUri, storageFileName);
             }
             else
             {
@@ -50,68 +41,61 @@ namespace XBMCRemoteRT.Converters
             return imageURI;
         }
 
-        private async void CheckImageStorage(string storagePath, Uri imageUri)
+        // TODO: Externalize these methods so the same logic can be applied to StringToImageBrushConverter
+        private async void CacheImage(Uri imageUri, string storageFileName)
         {
-            string filename = Path.GetFileName(storagePath);
-            string[] path = Path.GetDirectoryName(storagePath).Split('\\');
+            string filename = storageFileName;
 
-            // Traverse directories to file location
+            // Attempt to open file in app temp folder
             StorageFolder parent = ApplicationData.Current.TemporaryFolder;
+            bool fileExists = false;
             try
             {
-                foreach (string dir in path)
-                {
-                    bool dirExists = false;
-                    try
-                    {
-                        parent = await parent.GetFolderAsync(dir);
-                        dirExists = true;
-                    }
-                    catch (FileNotFoundException)
-                    {
-                        // Image is not cached, continue creating directories
-                    }
-
-                    if (!dirExists)
-                    {
-                        parent = await parent.CreateFolderAsync(dir);
-                        System.Diagnostics.Debug.WriteLine("Created " + parent.Path);
-                    }
-                }
-
-                // Attempt to open file
-                bool fileExists = false;
-                try
-                {
-                    await parent.GetFileAsync(filename);
-                    fileExists = true;
-                    // TODO: Check file age and refresh cached image. Age is a bad way to do it though.
-                }
-                catch (FileNotFoundException)
-                {
-                    // Image is not cached, download and save file
-                }
-                if (!fileExists)
-                {
-                    // TODO: Can I create the file only after request is complete? That way we avoid empty files from aborted requests
-                    // or it might be okay actually because file isn't closed
-                    StorageFile file = await parent.CreateFileAsync(filename);
-                    System.Diagnostics.Debug.WriteLine("Creating " + file.Path);
-                    GetImage(imageUri, file);
-                }
+                await parent.GetFileAsync(filename);
+                fileExists = true;
+                // TODO: Check file age and refresh cached image. Age is a bad way to do it though.
             }
-            catch (ArgumentException)
+            catch (FileNotFoundException)
             {
-                // Invalid characters in folder or file name. See 
-                // Path.GetInvalid(Path|FileName)Chars. We could sanitize the 
-                // path, but since the paths should also be valid on the host
-                // we just swallow the exception and the image won't be
-                // downloaded.
+                // Image is not cached, download and save file
+            }
+            if (!fileExists)
+            {
+                System.Diagnostics.Debug.WriteLine("Creating " + filename);
+                Stream imageStream = await GetImage(imageUri);
+
+                if (imageStream != null)
+                {
+                    // Prepare input image stream
+                    IInputStream inStream = imageStream.AsInputStream();
+                    DataReader reader = new DataReader(inStream);
+
+                    // Prepare output file stream
+                    StorageFile file = await parent.CreateFileAsync(filename);
+                    IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.ReadWrite);
+
+                    // Buffered write to file
+                    await reader.LoadAsync(1024);
+                    while (reader.UnconsumedBufferLength > 0)
+                    {
+                        await fileStream.WriteAsync(reader.ReadBuffer(reader.UnconsumedBufferLength));
+                        await reader.LoadAsync(1024);
+                    }
+
+                    await fileStream.FlushAsync();
+                    inStream.Dispose();
+                    // TODO: Handle write task canceled exceptions, or prepare for corrupted images
+                    // TODO: If this image was just created, it is likely that the
+                    // ImageSource consuming the path aborted due to file not 
+                    // found. Can we "refresh" the ImageSource?
+                }
             }
         }
 
-        private async void GetImage(Uri imageUri, StorageFile outFile)
+        private async Task<Stream> GetImage(Uri imageUri)
         {
+            Stream imageStream = null;
+
             // Download the image with HTTP Basic auth
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(imageUri.Scheme + "://" + imageUri.Authority);
@@ -130,25 +114,12 @@ namespace XBMCRemoteRT.Converters
             }
 
             HttpResponseMessage res = await client.SendAsync(req);
-            // TODO: Ensure requests are cached
             if (res.IsSuccessStatusCode)
             {
-                IInputStream imageStream = (await res.Content.ReadAsStreamAsync()).AsInputStream();
-                DataReader reader = new DataReader(imageStream);
-
-                IRandomAccessStream fileStream = await outFile.OpenAsync(FileAccessMode.ReadWrite);
-
-                await reader.LoadAsync(1024);
-                while (reader.UnconsumedBufferLength > 0)
-                {
-                    await fileStream.WriteAsync(reader.ReadBuffer(reader.UnconsumedBufferLength));
-                    await reader.LoadAsync(1024);
-                }
-
-                await fileStream.FlushAsync();
-                imageStream.Dispose();
+                imageStream = await res.Content.ReadAsStreamAsync();
             }
-            // TODO: Handle failed download by ensuring file is not saved
+
+            return imageStream;
         }
 
 
