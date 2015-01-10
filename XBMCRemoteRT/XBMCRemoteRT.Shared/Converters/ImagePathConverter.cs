e755309f7243a5ec409d02d59ac849105270b696
Christian Prescott <me@christianprescott.com>
2015-01-09 21:12:20 -0500

    Extract image cache methods from ImagePathConverter
    Create CacheManager
    Move image download, hash, and file I/O to CacheManager
    Create CacheManager method stubs

--- XBMCRemoteRT/XBMCRemoteRT.Shared/Converters/ImagePathConverter.cs
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Converters/ImagePathConverter.cs
@@ -17,116 +17,30 @@ namespace XBMCRemoteRT.Converters
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             string imagePath = (value == null) ? string.Empty : (string)value;
-            string imageURL = string.Empty;
+            Uri imageURI = null;
             string proxyScheme = "image://";
-            // TODO: are there other possible schemes for imagePath?
             if (imagePath.StartsWith(proxyScheme))
             {
-                // Build Kodi proxy image address
-                var encodedUri = WebUtility.UrlEncode(imagePath);
-                string baseUrlString = "http://" + ConnectionManager.CurrentConnection.IpAddress + ":" + ConnectionManager.CurrentConnection.Port.ToString() + "/image/";
-                imageURL = baseUrlString + encodedUri;
-
                 // Only apply cache logic if authentication is in use. If not,
                 // allow the image to be consumed from Kodi.
+                // TODO: Apply same logic to StringToImageBrushConverter
                 if (ConnectionManager.CurrentConnection.Password != String.Empty)
                 {
-                    Uri imageUri = new Uri(imageURL);
-
-                    // Hash the image proxy path into local storage file name
-                    string storageFileName = MD5Core.GetHashString(imagePath) + ".tmp";
-                    imageURL = ApplicationData.Current.TemporaryFolder.Path + '\\' + storageFileName;
-
-                    CacheImage(imageUri, storageFileName);
+                    // Get path to locally cached image
+                    imageURI = CacheManager.GetCacheUri(imagePath);
+                }
+                else
+                {
+                    // Get Kodi proxy image address
+                    imageURI = CacheManager.GetRemoteUri(imagePath);
                 }
             }
             else
             {
-                imageURL = "ms-appx:///Assets/DefaultArt.jpg";
+                imageURI = new Uri("ms-appx:///Assets/DefaultArt.jpg", UriKind.Absolute);
             }
-            Uri imageURI = new Uri(imageURL, UriKind.Absolute);
             return imageURI;
         }
-
-        // TODO: Externalize these methods so the same logic can be applied to StringToImageBrushConverter
-        private async void CacheImage(Uri imageUri, string storageFileName)
-        {
-            string filename = storageFileName;
-
-            // Attempt to open file in app temp folder
-            StorageFolder parent = ApplicationData.Current.TemporaryFolder;
-            bool fileExists = false;
-            try
-            {
-                await parent.GetFileAsync(filename);
-                fileExists = true;
-                // TODO: Check file age and refresh cached image. Age is a bad way to do it though.
-            }
-            catch (FileNotFoundException)
-            {
-                // Image is not cached, download and save file
-            }
-            if (!fileExists)
-            {
-                Stream imageStream = await GetImage(imageUri);
-
-                if (imageStream != null)
-                {
-                    // Prepare input image stream
-                    IInputStream inStream = imageStream.AsInputStream();
-                    DataReader reader = new DataReader(inStream);
-
-                    // Prepare output file stream
-                    StorageFile file = await parent.CreateFileAsync(filename);
-                    IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.ReadWrite);
-
-                    // Buffered write to file
-                    await reader.LoadAsync(1024);
-                    while (reader.UnconsumedBufferLength > 0)
-                    {
-                        await fileStream.WriteAsync(reader.ReadBuffer(reader.UnconsumedBufferLength));
-                        await reader.LoadAsync(1024);
-                    }
-
-                    await fileStream.FlushAsync();
-                    inStream.Dispose();
-                    // TODO: Handle write task canceled exceptions, or prepare for corrupted images
-                    // TODO: If this image was just created, it is likely that the
-                    // ImageSource consuming the path aborted due to file not 
-                    // found. Can we "refresh" the ImageSource?
-                }
-            }
-        }
-
-        private async Task<Stream> GetImage(Uri imageUri)
-        {
-            Stream imageStream = null;
-
-            // Download the image with HTTP Basic auth
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(imageUri.Scheme + "://" + imageUri.Authority);
-            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, imageUri.AbsolutePath);
-            if (ConnectionManager.CurrentConnection.Password != String.Empty)
-            {
-                req.Headers.Authorization = new AuthenticationHeaderValue(
-                    "Basic",
-                    System.Convert.ToBase64String(Encoding.UTF8.GetBytes(
-                        String.Format("{0}:{1}",
-                        ConnectionManager.CurrentConnection.Username,
-                        ConnectionManager.CurrentConnection.Password)
-                    ))
-                );
-            }
-
-            HttpResponseMessage res = await client.SendAsync(req);
-            if (res.IsSuccessStatusCode)
-            {
-                imageStream = await res.Content.ReadAsStreamAsync();
-            }
-
-            return imageStream;
-        }
-
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
---
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Helpers/CacheManager.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using XBMCRemoteRT.Models;
+
+namespace XBMCRemoteRT.Helpers
+{
+    public class CacheManager
+    {
+        public static async void InitCacheAsync()
+        {
+            IEnumerable<string> imagePaths = await GetAllImagesAsync();
+            foreach (string imagePath in imagePaths)
+            {
+                Stream imageStream = await GetImageStream(GetRemoteUri(imagePath));
+                if (imageStream != null)
+                {
+                    // TODO: Save image
+                }
+                else
+                {
+                    // TODO: Handle failed image downloads with friendly error message
+                }
+            }
+        }
+
+        public static async Task<IEnumerable<string>> GetAllImagesAsync()
+        {
+            List<string> imagePaths = new List<string>();
+
+            // TODO: Compile a collection of potential images from movies, music, and TV.
+
+            return imagePaths;
+        }
+
+        /// <summary>
+        /// Returns remote location of the image on the currently connected Kodi server.
+        /// </summary>
+        /// <param name="imagePath">Path on remote server to the resource. Usually begins with "image://"</param>
+        /// <returns>Location of the resource on the currently connected Kodi server. null if not connected or if imagePath is invalid.</returns>
+        public static Uri GetRemoteUri(string imagePath)
+        {
+            Uri imageUri = null;
+
+            // Build Kodi proxy image address
+            ConnectionItem con = ConnectionManager.CurrentConnection;
+            if (con != null)
+            {
+                string baseUrlString = "http://" + con.IpAddress + ":" + con.Port.ToString() + "/image/";
+                string encodedUri = WebUtility.UrlEncode(imagePath);
+                try
+                {
+                    imageUri = new Uri(baseUrlString + encodedUri);
+                }
+                catch (FormatException) { }
+            }
+            return imageUri;
+        }
+
+        /// <summary>
+        /// Returns path to a locally cached copy of the image. If the imagePath has not yet been cached, download is initiated but the Uri remains the same.
+        /// </summary>
+        /// <param name="imagePath">Path on remote server to the resource. Usually begins with "image://"</param>
+        /// <returns>Path to locally cached copy of the resource</returns>
+        public static Uri GetCacheUri(string imagePath)
+        {
+            Uri imageUri = null;
+
+            // TODO: Should each connection have its own cache? Two connections could have different images at the same path, but unlikely.
+            string storageFileName = MD5Core.GetHashString(imagePath) + ".tmp";
+            string storagePath = Path.Combine(ApplicationData.Current.TemporaryFolder.Path, storageFileName);
+            imageUri = new Uri(storagePath, UriKind.Absolute);
+
+            // TODO: Ideally, we've predicted all the possible images and cached them, but cache misses can be handled by simply downloading the image to the cache. Only a mild inconvenience for the user.
+            VerifyCache(storageFileName);
+
+            return imageUri;
+        }
+
+        private static async void VerifyCache(string filename)
+        {
+            if (!(await IsFileCached(filename)))
+            {
+                // TODO: cache miss. download and save.
+                // TODO: Check file age and refresh cached image. Age is a bad way to do it though.
+            }
+        }
+
+        private static async Task<bool> IsFileCached(string filename)
+        {
+
+            // Attempt to open file in app temp folder
+            StorageFolder parent = ApplicationData.Current.TemporaryFolder;
+            bool fileExists = false;
+            try
+            {
+                await parent.GetFileAsync(filename);
+                fileExists = true;
+            }
+            catch (FileNotFoundException)
+            {
+                // Image is not cached
+            }
+            return fileExists;
+        }
+
+        /// <summary>
+        /// Downloads image content from a remote location. TODO: Requests are cached.
+        /// </summary>
+        /// <param name="imageUri">Image location</param>
+        /// <returns>Stream content of the HTTP response. null if not connected, imageUri is invalid, or HTTP response is not OK</returns>
+        private static async Task<Stream> GetImageStream(Uri imageUri)
+        {
+            Stream imageStream = null;
+
+            // Download the image with HTTP Basic auth
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(imageUri.Scheme + "://" + imageUri.Authority);
+            HttpRequestMessage req = new HttpRequestMessage(HttpMethod.Get, imageUri.AbsolutePath);
+            ConnectionItem con = ConnectionManager.CurrentConnection;
+            if (con != null && con.Password != String.Empty)
+            {
+                req.Headers.Authorization = new AuthenticationHeaderValue(
+                    "Basic",
+                    System.Convert.ToBase64String(Encoding.UTF8.GetBytes(
+                        String.Format("{0}:{1}",
+                        con.Username,
+                        con.Password)
+                    ))
+                );
+            }
+
+            HttpResponseMessage res = await client.SendAsync(req);
+            if (res.IsSuccessStatusCode)
+            {
+                imageStream = await res.Content.ReadAsStreamAsync();
+            }
+
+            return imageStream;
+        }
+
+        private static async void WriteFile(Stream stream, string filename)
+        {
+
+            // Prepare input image stream
+            IInputStream inStream = stream.AsInputStream();
+            DataReader reader = new DataReader(inStream);
+
+            // Prepare output file stream
+            StorageFolder parent = ApplicationData.Current.TemporaryFolder;
+            StorageFile file = null;
+            try
+            {
+                file = await parent.CreateFileAsync(filename, CreationCollisionOption.ReplaceExisting);
+            }
+            catch (Exception) {
+            }
+
+            if (file != null)
+            {
+                IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.ReadWrite);
+                // Buffered write to file
+                await reader.LoadAsync(1024);
+                while (reader.UnconsumedBufferLength > 0)
+                {
+                    await fileStream.WriteAsync(reader.ReadBuffer(reader.UnconsumedBufferLength));
+                    await reader.LoadAsync(1024);
+                }
+
+                await fileStream.FlushAsync();
+                inStream.Dispose();
+                // TODO: Handle write task canceled exceptions, or prepare for corrupted images
+            }
+        }
+
+        public static async void ClearCacheAsync()
+        {
+            // TODO: Empty the temp folder for the current connection
+        }
+    }
+}
