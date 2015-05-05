0bbac5f3ce1a4c60a4a8512a12e131d7bc3c543b
Akshay Zade <akshay2000@hotmail.com>
2015-05-05 19:42:38 +0530

    Added empty stubs for Files APIs

---
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Models/Files/File.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XBMCRemoteRT.Models.Files
+{
+    public class File
+    {
+    }
+}
---
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/RPCWrappers/Files.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XBMCRemoteRT.Models.Common;
+using XBMCRemoteRT.Models.Files;
+
+namespace XBMCRemoteRT.RPCWrappers
+{
+    public class Files
+    {
+        public static Task<List<File>> GetDirectory(string directory, string media = "files", Sort sort = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
