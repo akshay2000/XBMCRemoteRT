049b0493e8d6f274b8a29d6b4fb8ce2c7258cc02
Akshay Zade <akshay2000@hotmail.com>
2015-07-14 22:26:36 +0530

    Disabled playlist button for non audio players

--- XBMCRemoteRT/XBMCRemoteRT.Shared/Converters/PlayerTypeToBoolConverter.cs
+++ XBMCRemoteRT/XBMCRemoteRT.Shared/Converters/PlayerTypeToBoolConverter.cs
@@ -11,7 +11,11 @@ namespace XBMCRemoteRT.Converters
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             Players player = (Players)value;
-            return player != Players.None;
+            string button = parameter == null ? string.Empty : parameter.ToString();
+            if (button == "playlist")
+                return player == Players.Audio && player != Players.None;
+            else
+                return player != Players.None;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
